08505e5dd63346d216f25d89f4d2a89cf51e8387
jje0ngyu <naniaerang@naver.com>
2025-01-07 00:19:44 +0900

    프로젝[INIT]í프 ì초기 생ì성

---
+++ src/main/java/com/hangheacinema/HangheaCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.hangheacinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangheaCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangheaCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hangheacinema/HangheaCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangheacinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangheaCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
